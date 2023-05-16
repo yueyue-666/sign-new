e60547b1f803da31d3cc213c7fdc46e572f5b96d
yueyue <yueyue.svip@gmail.com>
2023-05-16 17:19:04 +0800

    下级充值记录

--- src/views/home/frontpage/index.vue
+++ src/views/home/frontpage/index.vue
@@ -1,3 +1,200 @@
+<!-- 统计卡片 -->
 <template>
-  <div>仪表盘</div>
+  <a-row :gutter="16" style="padding: 0 20px;">
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="blue" shape="circle" size="large">
+          <eye-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.todayCharged }}</h1>
+        <div class="monitor-count-card-text">今日充值</div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="orange" shape="circle" size="large">
+          <fire-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.todayUsed }}</h1>
+        <div class="monitor-count-card-text">今日消耗</div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="red" shape="circle" size="large">
+          <flag-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.yesterdayCharged }}</h1>
+        <div class="monitor-count-card-text">昨日充值</div>
+        <div class="monitor-count-card-trend ele-text-danger"></div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="green" shape="circle" size="large">
+          <thunderbolt-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.todayCharged }}</h1>
+        <div class="monitor-count-card-text">昨日消耗</div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="blue" shape="circle" size="large">
+          <eye-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.thirtyDayUsed }}</h1>
+        <div class="monitor-count-card-text">iOS三十日消耗量</div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="orange" shape="circle" size="large">
+          <fire-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.androidThirtyDayUsed }}</h1>
+        <div class="monitor-count-card-text">Android三十日消耗量</div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="red" shape="circle" size="large">
+          <flag-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.appCount }}</h1>
+        <div class="monitor-count-card-text">iOS应用数量</div>
+        <div class="monitor-count-card-trend ele-text-danger"></div>
+      </a-card>
+    </a-col>
+    <a-col :lg="6" :md="12" :sm="12" :xs="24" style="margin-top: 20px;">
+      <a-card :bordered="false" class="monitor-count-card">
+        <ele-tag color="green" shape="circle" size="large">
+          <thunderbolt-filled />
+        </ele-tag>
+        <h1 class="monitor-count-card-num">{{superSign.androidAppCount }}</h1>
+        <div class="monitor-count-card-text">Android应用数量</div>
+      </a-card>
+    </a-col>
+
+    <a-col :lg="24" :md="24" :sm="24" :xs="24" style="padding-top: 20px;">
+      <a-card title="超级签名V2" :bordered="false" style="height: 96%;">
+        <ele-dashboard type="success" style="margin-right: 18px">
+          <div style="line-height: 1">
+            <span style="font-size: 48px">{{superSign.todayCharged }}</span>
+          </div>
+          <div style="margin-top: 4px">今日充值量</div>
+        </ele-dashboard>
+        <ele-dashboard type="warning" style="margin-right: 18px">
+          <div style="line-height: 1">
+            <span style="font-size: 48px">{{superSign.sevenDayCharged }}</span>
+          </div>
+          <div style="margin-top: 4px">七日充值量</div>
+        </ele-dashboard>
+        <ele-dashboard type="danger" style="margin-right: 18px">
+          <div style="line-height: 1">
+            <span style="font-size: 48px">{{ superSign.thirtyDayCharged }}</span>
+          </div>
+          <div style="margin-top: 4px">三十日充值量</div>
+        </ele-dashboard>
+        <ele-dashboard type="info" style="margin-right: 18px">
+          <div style="line-height: 1">
+            <span style="font-size: 48px">{{superSign.todayUsed }}</span>
+          </div>
+          <div style="margin-top: 4px">今日签发消耗量</div>
+        </ele-dashboard>
+        <ele-dashboard type="warning" style="margin-right: 18px">
+          <div style="line-height: 1">
+            <span style="font-size: 48px">{{superSign.sevenDayUsed }}</span>
+          </div>
+          <div style="margin-top: 4px">七日签发消耗量</div>
+        </ele-dashboard>
+        <ele-dashboard type="danger">
+          <div style="line-height: 1">
+            <span style="font-size: 48px">{{ superSign.thirtyDayUsed }}</span>
+          </div>
+          <div style="margin-top: 4px">三十日签发消耗量</div>
+        </ele-dashboard>
+      </a-card>
+    </a-col>
+    <!-- <a-col :lg="14" :md="12" :sm="12" :xs="24">
+        <VisitHour />
+    </a-col>-->
+  </a-row>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import request from '@/utils/request';
+
+// import VisitHour from './visit-hour.vue';
+
+import {
+  QuestionCircleOutlined,
+  EyeFilled,
+  FireFilled,
+  FlagFilled,
+  ThunderboltFilled,
+  UpOutlined,
+  DownOutlined
+} from '@ant-design/icons-vue';
+
+const superSign = ref({});
+
+// androidAppCount
+// androidThirtyDayUsed
+// appCount
+// sevenDayCharged
+// sevenDayUsed
+// thirtyDayCharged
+// thirtyDayUsed
+// thirtyDayUsedForOtherIds
+// todayCharged
+// todayUsed
+// todayUsedForOtherIds
+// yesterdayCharged
+// yesterdayUsed
+// yesterdayUsedForOtherIds
+
+request
+  .post('/backstage/superSignRecords', {})
+  .then((res) => {
+    superSign.value = res.data.data;
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+</script>
+
+<style lang="less" scoped>
+.monitor-count-card {
+  text-align: center;
+
+  .monitor-count-card-num {
+    margin-top: 6px;
+    font-size: 32px;
+  }
+
+  .monitor-count-card-text {
+    font-size: 12px;
+    margin: 8px 0;
+    opacity: 0.8;
+  }
+
+  .monitor-count-card-trend {
+    font-weight: bold;
+    line-height: 26px;
+
+    & > .anticon {
+      margin-right: 6px;
+    }
+  }
+
+  .monitor-count-card-tips {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    cursor: pointer;
+    opacity: 0.6;
+  }
+}
+</style>
--- src/views/home/transactionmanagement/Subordinaterecharge/index.vue
+++ src/views/home/transactionmanagement/Subordinaterecharge/index.vue
@@ -1,3 +1,335 @@
 <template>
-  <div>下级充值记录</div>
+  <div class="ele-body">
+    <a-card :bordered="false">
+      <!-- 搜索 -->
+      <a-form :label-col="{ xl: 7, lg: 5, md: 7, sm: 4 }" :wrapper-col="{ xl: 17, lg: 19, md: 17, sm: 20 }">
+        <a-row :gutter="8">
+          <a-col :xl="5" :lg="12" :md="12" :sm="24" :xs="24">
+            <a-form-item>
+              <a-select v-model:value="form.type" placeholder="请选择" allow-clear>
+                <a-select-option value="0">超级签V2</a-select-option>
+                <a-select-option value="1">超级签V3</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="5" :lg="12" :md="12" :sm="24" :xs="24">
+            <a-form-item>
+              <a-input allow-clear :maxlength="20" placeholder="用户" v-model:value="form.username" />
+            </a-form-item>
+          </a-col>
+          <a-col :xl="4" :lg="12" :md="12" :sm="24" :xs="24">
+            <a-form-item label="日期">
+              <a-date-picker class="ele-fluid" placeholder="请选择" value-format="YYYY-MM-DD HH:mm:ss" v-model:value="form.startTime" />
+            </a-form-item>
+          </a-col>
+          <a-col :xl="4" :lg="12" :md="12" :sm="24" :xs="24">
+            <a-form-item>
+              <a-date-picker class="ele-fluid" placeholder="请选择" value-format="YYYY-MM-DD HH:mm:ss" v-model:value="form.endTime" />
+            </a-form-item>
+          </a-col>
+          <a-col :xl="4" :lg="12" :md="12" :sm="24" :xs="24">
+            <a-form-item class="ele-text-right" :wrapper-col="{ span: 24 }">
+              <a-space>
+                <a-button type="primary" @click="reload">查询</a-button>
+                <a-button @click="reset">重置</a-button>
+              </a-space>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+      <!-- 表格 -->
+      <ele-pro-table
+        ref="tableRef"
+        row-key="userId"
+        :columns="columns"
+        :datasource="datasource"
+        :scroll="{ x: 1000 }"
+        :where="defaultWhere"
+        cache-key="proSystemUserTable"
+      >
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'statuss'">
+            <a-tag v-if="record.statuss === 0" color="green">处理成功</a-tag>
+            <span v-else>-</span>
+          </template>
+          <template v-else-if="column.key === 'userName'">
+            <a @click="openyonghu(record)">{{ record.userName }}</a>
+          </template>
+          <template v-else-if="column.key === 'action'">
+            <a-space>
+              <a @click="mark(record)">备注</a>
+              <a-divider type="vertical" />
+              <a class="ele-text-danger" v-if="record.isDeleted">已删除</a>
+              <a-popconfirm placement="topRight" title="确定要删除此消息吗？" @confirm="remove(record)" v-else>
+                <a class="ele-text-danger">删除</a>
+              </a-popconfirm>
+            </a-space>
+          </template>
+        </template>
+      </ele-pro-table>
+    </a-card>
+
+    <!-- 备注弹窗 -->
+    <ele-modal
+      :width="400"
+      title="设置备注"
+      v-model:visible="visible1"
+      :resizable="true"
+      :maxable="true"
+      @cancel="visible1=false"
+      @ok="remarksave"
+    >
+      <a-form ref="remarkformRef" :model="remarkform" :label-col="{ flex: '70px' }" :wrapper-col="{ flex: '1' }">
+        <a-form-item label="备注" style="flex-wrap: nowrap">
+          <a-textarea :rows="4" placeholder="请输入备注" v-model:value="remarkform.remark" />
+        </a-form-item>
+      </a-form>
+    </ele-modal>
+  </div>
 </template>
+
+<script setup>
+import { ref, reactive, nextTick } from 'vue';
+import { toDateString } from 'ele-admin-pro/es';
+import useFormData from '@/utils/use-form-data';
+import request from '@/utils/request';
+import { message, Modal } from 'ant-design-vue/es';
+import { AllUserOrderByPage } from '@/api/system/user';
+import { removePageTab } from '@/utils/page-tab-util';
+import { useRouter } from 'vue-router';
+// 表格实例
+const tableRef = ref(null);
+
+// 表格列配置
+const columns = ref([
+  {
+    title: '流水号',
+    dataIndex: 'orderNum',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false
+  },
+  {
+    title: '用户',
+    key: 'userName',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false
+  },
+  {
+    title: '数量',
+    dataIndex: 'amount',
+    align: 'center',
+    width: 120,
+    ellipsis: true
+  },
+  {
+    title: '金额',
+    dataIndex: 'amount',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false,
+    customRender: ({ text }) => '-'
+  },
+  {
+    title: '类型',
+    dataIndex: 'type',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false,
+    customRender: ({ text }) => {
+      if (text === 0) {
+        return '超级签V2';
+      } else if (text === 1) {
+        return '应用分发';
+      } else {
+        return '-';
+      }
+    }
+  },
+  {
+    title: '状态',
+    key: 'statuss',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false
+  },
+  {
+    title: '后台备注',
+    dataIndex: 'remark',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false,
+    customRender: ({ text }) => {
+      if (text) {
+        return text;
+      } else {
+        return '-';
+      }
+    }
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTimestamp',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false
+  },
+  {
+    title: '处理时间',
+    dataIndex: 'updateTimestamp',
+    align: 'center',
+    sorter: false,
+    showSorterTooltip: false
+  },
+  {
+    title: '操作',
+    key: 'action',
+    width: 200,
+    fixed: 'right',
+    align: 'center'
+  }
+]);
+
+const appList = ref({});
+// 获取应用下拉
+const getselect = () => {
+  let body = { isCheckSub: 'false', userId: localStorage.getItem('userId') };
+  request
+    .post('/backstage/getAppList', body)
+    .then((res) => {
+      appList.value = res.data.data;
+    })
+    .catch((e) => {
+      message.error(e.response.data.msg);
+    });
+};
+const { push } = useRouter();
+// 打开用户界面
+const openyonghu = (row) => {
+  const path = '/home/UserManagemen';
+  removePageTab({ key: path });
+  nextTick(() => {
+    push({
+      path,
+      query: row ? { userName: row.userName } : undefined
+    });
+  });
+};
+
+// 日期范围选择
+var date = new Date();
+// 当前时间
+var s1 =
+  date.getFullYear() +
+  '-' +
+  ((date.getMonth() + 1 < 10
+    ? '0' + (date.getMonth() + 1)
+    : date.getMonth() + 1) +
+    '-') +
+  date.getDate();
+// 7天前的时间
+date.setTime(date.getTime() - 480 * 60 * 60 * 1000);
+var s2 =
+  date.getFullYear() +
+  '-' +
+  ((date.getMonth() + 1 < 10
+    ? '0' + (date.getMonth() + 1)
+    : date.getMonth() + 1) +
+    '-') +
+  (date.getDate() + 1 < 10 ? '0' + (date.getDate() + 1) : date.getDate() + 1);
+
+// 表单数据
+const { form, resetFields } = useFormData({
+  type: '0',
+  isCheckSub: true,
+  startTime: s2 + ' 00:00:00',
+  endTime: s1 + ' 23:59:59'
+});
+
+// /* 搜索 */
+// const search = () => {
+//   emit('search', form);
+// };
+
+// 删除
+const remove = (row) => {
+  let body = { appId: row.appId };
+  request
+    .post('/backstage/deleteOrderRecord', body)
+    .then((res) => {
+      reload();
+      message.error(res.data.msg);
+    })
+    .catch((e) => {
+      message.error(e.response.data.msg);
+    });
+};
+
+/*  重置 */
+const reset = () => {
+  resetFields();
+};
+
+// 默认搜索条件
+const defaultWhere = reactive({
+  page: 1,
+  size: 20
+});
+
+// 表格数据源
+const datasource = ({ page, limit, where, orders }) => {
+  return AllUserOrderByPage({ ...form, ...orders, page, size: limit });
+};
+
+/* 搜索 */
+const reload = (where) => {
+  tableRef?.value?.reload({ page: 1, ...form });
+};
+
+// 点击备注
+const mark = (row) => {
+  visible1.value = true;
+  remarkform.id = row.id;
+  remarkform.remark = row.remark;
+};
+
+//
+const remarkformRef = ref(null);
+
+// 是否显示备注弹窗
+const visible1 = ref(false);
+
+// 备注数据
+const remarkform = reactive({
+  remark: '',
+  id: ''
+});
+
+// 更改备注
+const remarksave = () => {
+  if (!remarkform.remark) {
+    message.info('请输入备注');
+    return;
+  }
+  let body = { id: remarkform.id, remark: remarkform.remark };
+  request
+    .post('/backstage/modifyOrderRecordRemark', body)
+    .then((res) => {
+      visible1.value = false;
+      message.error(res.data.msg);
+      reload();
+    })
+    .catch((e) => {
+      message.error(e.response.data.msg);
+    });
+};
+
+getselect();
+</script>
+
+<script>
+export default {
+  name: 'listPage'
+};
+</script>
